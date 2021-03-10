faf4e548ee34f4f62f043b33a4d013dcdd805a26
Talisson Sozinho <talissonlutero8080@gmail.com>
2021-03-09 22:13:09 -0300

    challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,30 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if( !newTaskTitle ) return;
+
+    const newTask:Task = {
+      id: Math.round(Math.random() * 1000000), 
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks( oldState => [...oldState, newTask] );
+    setNewTaskTitle("");
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const changeIsCompleteTask:Task[] = tasks.map( task => task.id === id ? { ...task, isComplete: !task.isComplete } : task);
+
+    setTasks(changeIsCompleteTask)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTask:Task[] = tasks.filter( task => task.id !== id)
+
+    setTasks( filteredTask )
   }
 
   return (
